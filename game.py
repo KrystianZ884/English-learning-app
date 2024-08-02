497f7967713a5161c50cbff7886d6293ac5c2e97
Krystian Zielinski <kzielinski884@gmail.com>
2024-08-03 01:26:07 +0200

    added feedback on correct or wrong answer

--- game.py
+++ game.py
@@ -13,17 +13,22 @@ def main_game():
         if random_int == 0:
             shot = input(f"{eng_txt}: ")
             if shot == pol_txt:
+                print("Correct")
                 points += 1
                 tries += 1
             else:
+                print(f"Error: {pol_txt}")
                 tries += 1
+
 
         if random_int == 1:
             shot = input(f"{pol_txt}: ")
             if shot == eng_txt:
+                print("Correct")
                 points += 1
                 tries += 1
             else:
+                print(f"Error: {eng_txt}")
                 tries += 1
 
         continue_choice = input("Would you like to continue?: [Y]/[N] ")
